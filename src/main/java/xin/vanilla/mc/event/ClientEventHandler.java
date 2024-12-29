77f68aaf4c936302c6fdaec7331fc43dfa030df8
Tsuki <TsukiMaaii@gmail.com>
2024-12-29 14:30:28 +0800

    chore 1.优化玩家背包界面按钮操作及显示

--- src/main/java/xin/vanilla/mc/event/ClientEventHandler.java
+++ src/main/java/xin/vanilla/mc/event/ClientEventHandler.java
@@ -110,64 +110,122 @@ public class ClientEventHandler {
     }
 
     @SubscribeEvent
-    public static void onRenderScreen(ScreenEvent.Init.Post event) {
+    public static void onRenderScreen(ScreenEvent event) {
         if (event.getScreen() instanceof EffectRenderingInventoryScreen) {
-            if (SakuraSignIn.getThemeTexture() == null) ClientEventHandler.loadThemeTexture();
-            // 创建按钮并添加到界面
-            String[] signInCoordinate = ClientConfig.INVENTORY_SIGN_IN_BUTTON_COORDINATE.get().split(",");
-            String[] rewardOptionCoordinate = ClientConfig.INVENTORY_REWARD_OPTION_BUTTON_COORDINATE.get().split(",");
-            double signInX_ = signInCoordinate.length == 2 ? StringUtils.toFloat(signInCoordinate[0]) : 0;
-            double signInY_ = signInCoordinate.length == 2 ? StringUtils.toFloat(signInCoordinate[1]) : 0;
-            double rewardOptionX_ = rewardOptionCoordinate.length == 2 ? StringUtils.toFloat(rewardOptionCoordinate[0]) : 0;
-            double rewardOptionY_ = rewardOptionCoordinate.length == 2 ? StringUtils.toFloat(rewardOptionCoordinate[1]) : 0;
+            if (event.isCancelable()) event.setCanceled(false);
+            if (event instanceof ScreenEvent.Init.Post) {
+                if (SakuraSignIn.getThemeTexture() == null) ClientEventHandler.loadThemeTexture();
+                // 创建按钮并添加到界面
+                String[] signInCoordinate = ClientConfig.INVENTORY_SIGN_IN_BUTTON_COORDINATE.get().split(",");
+                String[] rewardOptionCoordinate = ClientConfig.INVENTORY_REWARD_OPTION_BUTTON_COORDINATE.get().split(",");
+                double signInX_ = signInCoordinate.length == 2 ? StringUtils.toFloat(signInCoordinate[0]) : 0;
+                double signInY_ = signInCoordinate.length == 2 ? StringUtils.toFloat(signInCoordinate[1]) : 0;
+                double rewardOptionX_ = rewardOptionCoordinate.length == 2 ? StringUtils.toFloat(rewardOptionCoordinate[0]) : 0;
+                double rewardOptionY_ = rewardOptionCoordinate.length == 2 ? StringUtils.toFloat(rewardOptionCoordinate[1]) : 0;
 
-            double signInX = signInX_;
-            double signInY = signInY_;
-            double rewardOptionX = rewardOptionX_;
-            double rewardOptionY = rewardOptionY_;
+                double signInX = signInX_;
+                double signInY = signInY_;
+                double rewardOptionX = rewardOptionX_;
+                double rewardOptionY = rewardOptionY_;
 
-            // 如果坐标为0则设置默认坐标
-            if (signInX == 0) signInX = 2;
-            if (signInY == 0) signInY = 2;
-            if (rewardOptionX == 0) rewardOptionX = 20;
-            if (rewardOptionY == 0) rewardOptionY = 2;
+                // 如果坐标为0则设置默认坐标
+                if (signInX == 0) signInX = 2;
+                if (signInY == 0) signInY = 2;
+                if (rewardOptionX == 0) rewardOptionX = 20;
+                if (rewardOptionY == 0) rewardOptionY = 2;
 
-            // 如果坐标发生变化则保存到配置文件
-            if (signInX_ != signInX || signInY_ != signInY) {
-                ClientConfig.INVENTORY_SIGN_IN_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", signInX, signInY));
+                // 如果坐标发生变化则保存到配置文件
+                if (signInX_ != signInX || signInY_ != signInY) {
+                    ClientConfig.INVENTORY_SIGN_IN_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", signInX, signInY));
+                }
+                if (rewardOptionX_ != rewardOptionX || rewardOptionY_ != rewardOptionY) {
+                    ClientConfig.INVENTORY_REWARD_OPTION_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", rewardOptionX, rewardOptionY));
+                }
+
+                // 如果坐标为百分比则转换为像素坐标
+                if (signInX > 0 && signInX <= 1) signInX *= event.getScreen().width;
+                if (signInY > 0 && signInY <= 1) signInY *= event.getScreen().height;
+                if (rewardOptionX > 0 && rewardOptionX <= 1) rewardOptionX *= event.getScreen().width;
+                if (rewardOptionY > 0 && rewardOptionY <= 1) rewardOptionY *= event.getScreen().height;
+
+                // 转换为有效坐标
+                signInX = InventoryButton.getValidX(signInX, AbstractGuiUtils.ITEM_ICON_SIZE);
+                signInY = InventoryButton.getValidY(signInY, AbstractGuiUtils.ITEM_ICON_SIZE);
+                rewardOptionX = InventoryButton.getValidX(rewardOptionX, AbstractGuiUtils.ITEM_ICON_SIZE);
+                rewardOptionY = InventoryButton.getValidY(rewardOptionY, AbstractGuiUtils.ITEM_ICON_SIZE);
+
+                InventoryButton signInButton = new InventoryButton((int) signInX, (int) signInY,
+                        AbstractGuiUtils.ITEM_ICON_SIZE,
+                        AbstractGuiUtils.ITEM_ICON_SIZE,
+                        I18nUtils.get("key.sakura_sign_in.sign_in"))
+                        .setUV(SakuraSignIn.getThemeTextureCoordinate().getSignInBtnUV(), SakuraSignIn.getThemeTextureCoordinate().getTotalWidth(), SakuraSignIn.getThemeTextureCoordinate().getTotalHeight())
+                        .setOnClick((button) -> ClientEventHandler.openSignInScreen(event.getScreen()))
+                        .setOnDragEnd((coordinate) -> ClientConfig.INVENTORY_SIGN_IN_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", coordinate.getX(), coordinate.getY())));
+                InventoryButton rewardOptionButton = new InventoryButton((int) rewardOptionX, (int) rewardOptionY,
+                        AbstractGuiUtils.ITEM_ICON_SIZE,
+                        AbstractGuiUtils.ITEM_ICON_SIZE,
+                        I18nUtils.get("key.sakura_sign_in.reward_option"))
+                        .setUV(SakuraSignIn.getThemeTextureCoordinate().getRewardOptionBtnUV(), SakuraSignIn.getThemeTextureCoordinate().getTotalWidth(), SakuraSignIn.getThemeTextureCoordinate().getTotalHeight())
+                        .setOnClick((button) -> Minecraft.getInstance().setScreen(new RewardOptionScreen().setPreviousScreen(event.getScreen())))
+                        .setOnDragEnd((coordinate) -> ClientConfig.INVENTORY_REWARD_OPTION_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", coordinate.getX(), coordinate.getY())));
+                ((ScreenEvent.Init.Post) event).addListener(signInButton);
+                ((ScreenEvent.Init.Post) event).addListener(rewardOptionButton);
             }
-            if (rewardOptionX_ != rewardOptionX || rewardOptionY_ != rewardOptionY) {
-                ClientConfig.INVENTORY_REWARD_OPTION_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", rewardOptionX, rewardOptionY));
+            // 手动触发鼠标、键盘与渲染事件
+            else if (event instanceof ScreenEvent.KeyPressed.Post) {
+                event.getScreen().children().stream()
+                        .filter(button -> button instanceof InventoryButton)
+                        .forEach(button -> {
+                            boolean cancel = ((InventoryButton) button).keyPressed_(
+                                    ((ScreenEvent.KeyPressed.Post) event).getKeyCode(),
+                                    ((ScreenEvent.KeyPressed.Post) event).getScanCode(),
+                                    ((ScreenEvent.KeyPressed.Post) event).getModifiers()
+                            );
+                            if (event.isCancelable()) event.setCanceled(cancel);
+                        });
+            } else if (event instanceof ScreenEvent.KeyReleased.Post) {
+                event.getScreen().children().stream()
+                        .filter(button -> button instanceof InventoryButton)
+                        .forEach(button -> {
+                            boolean cancel = ((InventoryButton) button).keyReleased_(
+                                    ((ScreenEvent.KeyReleased.Post) event).getKeyCode(),
+                                    ((ScreenEvent.KeyReleased.Post) event).getScanCode(),
+                                    ((ScreenEvent.KeyReleased.Post) event).getModifiers()
+                            );
+                            if (event.isCancelable()) event.setCanceled(cancel);
+                        });
+            } else if (event instanceof ScreenEvent.MouseButtonPressed.Pre) {
+                event.getScreen().children().stream()
+                        .filter(button -> button instanceof InventoryButton)
+                        .forEach(button -> {
+                            boolean cancel = ((InventoryButton) button).mouseClicked_(
+                                    ((ScreenEvent.MouseButtonPressed.Pre) event).getMouseX(),
+                                    ((ScreenEvent.MouseButtonPressed.Pre) event).getMouseY(),
+                                    ((ScreenEvent.MouseButtonPressed.Pre) event).getButton()
+                            );
+                            if (event.isCancelable()) event.setCanceled(cancel);
+                        });
+            } else if (event instanceof ScreenEvent.MouseButtonReleased.Pre) {
+                event.getScreen().children().stream()
+                        .filter(button -> button instanceof InventoryButton)
+                        .forEach(button -> {
+                            boolean cancel = ((InventoryButton) button).mouseReleased_(
+                                    ((ScreenEvent.MouseButtonReleased.Pre) event).getMouseX(),
+                                    ((ScreenEvent.MouseButtonReleased.Pre) event).getMouseY(),
+                                    ((ScreenEvent.MouseButtonReleased.Pre) event).getButton()
+                            );
+                            if (event.isCancelable()) event.setCanceled(cancel);
+                        });
+            } else if (event instanceof ScreenEvent.Render.Post) {
+                event.getScreen().children().stream()
+                        .filter(button -> button instanceof InventoryButton)
+                        .forEach(button -> ((InventoryButton) button).render_(
+                                ((ScreenEvent.Render.Post) event).getPoseStack(),
+                                ((ScreenEvent.Render.Post) event).getMouseX(),
+                                ((ScreenEvent.Render.Post) event).getMouseY(),
+                                ((ScreenEvent.Render.Post) event).getPartialTick()
+                        ));
             }
-
-            // 如果坐标为百分比则转换为像素坐标
-            if (signInX > 0 && signInX <= 1) signInX *= event.getScreen().width;
-            if (signInY > 0 && signInY <= 1) signInY *= event.getScreen().height;
-            if (rewardOptionX > 0 && rewardOptionX <= 1) rewardOptionX *= event.getScreen().width;
-            if (rewardOptionY > 0 && rewardOptionY <= 1) rewardOptionY *= event.getScreen().height;
-
-            // 转换为有效坐标
-            signInX = InventoryButton.getValidX(signInX, AbstractGuiUtils.ITEM_ICON_SIZE);
-            signInY = InventoryButton.getValidY(signInY, AbstractGuiUtils.ITEM_ICON_SIZE);
-            rewardOptionX = InventoryButton.getValidX(rewardOptionX, AbstractGuiUtils.ITEM_ICON_SIZE);
-            rewardOptionY = InventoryButton.getValidY(rewardOptionY, AbstractGuiUtils.ITEM_ICON_SIZE);
-
-            InventoryButton signInButton = new InventoryButton((int) signInX, (int) signInY,
-                    AbstractGuiUtils.ITEM_ICON_SIZE,
-                    AbstractGuiUtils.ITEM_ICON_SIZE,
-                    I18nUtils.get("key.sakura_sign_in.sign_in"))
-                    .setUV(SakuraSignIn.getThemeTextureCoordinate().getSignInBtnUV(), SakuraSignIn.getThemeTextureCoordinate().getTotalWidth(), SakuraSignIn.getThemeTextureCoordinate().getTotalHeight())
-                    .setOnClick((button) -> ClientEventHandler.openSignInScreen(event.getScreen()))
-                    .setOnDragEnd((coordinate) -> ClientConfig.INVENTORY_SIGN_IN_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", coordinate.getX(), coordinate.getY())));
-            InventoryButton rewardOptionButton = new InventoryButton((int) rewardOptionX, (int) rewardOptionY,
-                    AbstractGuiUtils.ITEM_ICON_SIZE,
-                    AbstractGuiUtils.ITEM_ICON_SIZE,
-                    I18nUtils.get("key.sakura_sign_in.reward_option"))
-                    .setUV(SakuraSignIn.getThemeTextureCoordinate().getRewardOptionBtnUV(), SakuraSignIn.getThemeTextureCoordinate().getTotalWidth(), SakuraSignIn.getThemeTextureCoordinate().getTotalHeight())
-                    .setOnClick((button) -> Minecraft.getInstance().setScreen(new RewardOptionScreen().setPreviousScreen(event.getScreen())))
-                    .setOnDragEnd((coordinate) -> ClientConfig.INVENTORY_REWARD_OPTION_BUTTON_COORDINATE.set(String.format("%.6f,%.6f", coordinate.getX(), coordinate.getY())));
-            event.addListener(signInButton);
-            event.addListener(rewardOptionButton);
         }
     }
 
