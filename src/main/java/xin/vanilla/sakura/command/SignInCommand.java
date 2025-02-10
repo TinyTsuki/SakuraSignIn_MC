17f5ff0f0f810167e6624844320ad39111c9b9c8
Tsuki <TsukiMaaii@gmail.com>
2025-02-11 01:56:35 +0800

    fix 1.签到指令的补签与签到判断反啦!
    (cherry picked from commit ee5ead7a33b8e42a0c8503a0128c143ff981e595)

--- src/main/java/xin/vanilla/sakura/command/SignInCommand.java
+++ src/main/java/xin/vanilla/sakura/command/SignInCommand.java
@@ -124,9 +124,9 @@ public class SignInCommand {
                     long date = getRelativeLong(string, "date");
                     Date signDate = DateUtils.getDate(date);
                     if (DateUtils.toDateInt(signDate) == RewardManager.getCompensateDateInt()) {
-                        signInTimeList.add(new KeyValue<>(signDate, ESignInType.RE_SIGN_IN));
-                    } else {
                         signInTimeList.add(new KeyValue<>(signDate, ESignInType.SIGN_IN));
+                    } else {
+                        signInTimeList.add(new KeyValue<>(signDate, ESignInType.RE_SIGN_IN));
                     }
                 }
             } catch (IllegalArgumentException ignored) {
@@ -178,9 +178,9 @@ public class SignInCommand {
                     long date = getRelativeLong(string, "date");
                     Date signDate = DateUtils.getDate(date);
                     if (DateUtils.toDateInt(signDate) == RewardManager.getCompensateDateInt()) {
-                        signInTimeList.add(new KeyValue<>(signDate, ESignInType.RE_SIGN_IN));
-                    } else {
                         signInTimeList.add(new KeyValue<>(signDate, ESignInType.SIGN_IN));
+                    } else {
+                        signInTimeList.add(new KeyValue<>(signDate, ESignInType.RE_SIGN_IN));
                     }
                 }
             } catch (IllegalArgumentException ignored) {
