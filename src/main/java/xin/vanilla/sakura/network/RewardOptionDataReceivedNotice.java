3fcd43a0c95a94ee9d7b72241f496ca5852db8c5
Tsuki <TsukiMaaii@gmail.com>
2025-02-20 21:35:10 +0800

    chore 1.添加编辑、查看奖励配置、查看奖励内容的权限配置 2.添加奖励配置上传、下载成功与否的提示信息 3.优化客户端提示信息展现方式
    fix
    1.玩家死亡重生后语言信息丢失
    
    (cherry picked from commit b615d3498d7f73fbbd1d5ecfa1e966e1facac3fc)

---
+++ src/main/java/xin/vanilla/sakura/network/RewardOptionDataReceivedNotice.java
@@ -0,0 +1,41 @@
+package xin.vanilla.sakura.network;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+import xin.vanilla.sakura.enums.EI18nType;
+import xin.vanilla.sakura.screen.component.NotificationManager;
+import xin.vanilla.sakura.util.Component;
+
+import java.util.function.Supplier;
+
+public class RewardOptionDataReceivedNotice {
+    private final boolean success;
+
+    public RewardOptionDataReceivedNotice(boolean success) {
+        this.success = success;
+    }
+
+    public RewardOptionDataReceivedNotice(FriendlyByteBuf buf) {
+        this.success = buf.readBoolean();
+    }
+
+    public void toBytes(FriendlyByteBuf buf) {
+        buf.writeBoolean(this.success);
+    }
+
+    public static void handle(RewardOptionDataReceivedNotice packet, Supplier<NetworkEvent.Context> ctx) {
+        // 获取网络事件上下文并排队执行工作
+        ctx.get().enqueueWork(() -> {
+            String key;
+            if (packet.success) {
+                key = "reward_option_upload_success";
+            } else {
+                key = "reward_option_upload_failed";
+            }
+            Component component = Component.translatable(EI18nType.MESSAGE, key);
+            NotificationManager.getInstance().addNotification(new NotificationManager.Notification(component).setBgColor(0x88FF5555));
+        });
+        // 设置数据包已处理状态，防止重复处理
+        ctx.get().setPacketHandled(true);
+    }
+}
