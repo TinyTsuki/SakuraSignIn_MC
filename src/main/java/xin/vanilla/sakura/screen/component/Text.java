4f926ef7bafdd0516728dd55f38878d56b62a703
Tsuki <TsukiMaaii@gmail.com>
2025-02-20 21:35:10 +0800

    chore 1.添加编辑、查看奖励配置、查看奖励内容的权限配置 2.添加奖励配置上传、下载成功与否的提示信息 3.优化客户端提示信息展现方式
    fix
    1.玩家死亡重生后语言信息丢失
    
    (cherry picked from commit b615d3498d7f73fbbd1d5ecfa1e966e1facac3fc)

--- src/main/java/xin/vanilla/sakura/screen/component/Text.java
+++ src/main/java/xin/vanilla/sakura/screen/component/Text.java
@@ -59,7 +59,7 @@ public class Text {
         this.hoverText = Component.literal(text);
     }
 
-    private Text(Component text) {
+    public Text(Component text) {
         this.text = text;
         this.hoverText = text;
     }
