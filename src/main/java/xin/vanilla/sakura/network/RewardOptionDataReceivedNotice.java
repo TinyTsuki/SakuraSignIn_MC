b594b8709b7633d592e6d27a95989938f6db4619
Tsuki <TsukiMaaii@gmail.com>
2025-02-21 16:56:17 +0800

    chore 1.添加奖励配置页面提示：无权限编辑、查看 2.优化奖励配置同步至服务器的逻辑 3.修改README.MD文件中关于配置的描述
    (cherry picked from commit 70007ffedeba902de809acdb08a2763d06e10a6c)

--- src/main/java/xin/vanilla/sakura/network/RewardOptionDataReceivedNotice.java
+++ src/main/java/xin/vanilla/sakura/network/RewardOptionDataReceivedNotice.java
@@ -24,14 +24,13 @@ public class RewardOptionDataReceivedNotice {
     public static void handle(RewardOptionDataReceivedNotice packet, CustomPayloadEvent.Context ctx) {
         // 获取网络事件上下文并排队执行工作
         ctx.enqueueWork(() -> {
-            String key;
+            NotificationManager.Notification notification;
             if (packet.success) {
-                key = "reward_option_upload_success";
+                notification = new NotificationManager.Notification(Component.translatable(EI18nType.MESSAGE, "reward_option_upload_success"));
             } else {
-                key = "reward_option_upload_failed";
+                notification = new NotificationManager.Notification(Component.translatable(EI18nType.MESSAGE, "reward_option_upload_failed")).setBgColor(0x88FF5555);
             }
-            Component component = Component.translatable(EI18nType.MESSAGE, key);
-            NotificationManager.getInstance().addNotification(new NotificationManager.Notification(component).setBgColor(0x88FF5555));
+            NotificationManager.getInstance().addNotification(notification);
         });
         // 设置数据包已处理状态，防止重复处理
         ctx.setPacketHandled(true);
