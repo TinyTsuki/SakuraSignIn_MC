9ff88f758a48da0c80b8dcf53358faa8548e4393
Tsuki <TsukiMaaii@gmail.com>
2025-02-24 14:40:06 +0800

    fix 1.Component.toTextComponent的部分问题
    (cherry picked from commit cc2ecbaa53d2157a7cbea17e1d18e8ccff53e5e0)

--- src/main/java/xin/vanilla/sakura/util/Component.java
+++ src/main/java/xin/vanilla/sakura/util/Component.java
@@ -477,7 +477,7 @@ public class Component implements Cloneable, Serializable {
         if (!this.isColorEmpty()) {
             if (this.i18nType != EI18nType.PLAIN) {
                 String text = I18nUtils.getTranslation(I18nUtils.getKey(this.i18nType, this.text), languageCode);
-                String[] split = text.split(StringUtils.FORMAT_REGEX);
+                String[] split = text.split(StringUtils.FORMAT_REGEX, -1);
                 for (String s : split) {
                     components.add(net.minecraft.network.chat.Component.literal(s).withStyle(this.getStyle()));
                 }
@@ -517,7 +517,9 @@ public class Component implements Cloneable, Serializable {
                             }
                         }
                     }
-                    components.get(i).append(formattedArg.toTextComponent());
+                    if (components.size() > i) {
+                        components.get(i).append(formattedArg.toTextComponent());
+                    }
                     i++;
                 }
             } else {
