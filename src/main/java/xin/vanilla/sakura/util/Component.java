9934f2b82793bd8a2c76e26a4a23fc1038c59f53
Tsuki <TsukiMaaii@gmail.com>
2025-02-24 14:40:06 +0800

    fix 1.Component.toTextComponent的部分问题
    (cherry picked from commit cc2ecbaa53d2157a7cbea17e1d18e8ccff53e5e0)

--- src/main/java/xin/vanilla/sakura/util/Component.java
+++ src/main/java/xin/vanilla/sakura/util/Component.java
@@ -480,7 +480,7 @@ public class Component implements Cloneable, Serializable {
         if (!this.isColorEmpty()) {
             if (this.i18nType != EI18nType.PLAIN) {
                 String text = I18nUtils.getTranslation(I18nUtils.getKey(this.i18nType, this.text), languageCode);
-                String[] split = text.split(StringUtils.FORMAT_REGEX);
+                String[] split = text.split(StringUtils.FORMAT_REGEX, -1);
                 for (String s : split) {
                     components.add(new StringTextComponent(s).withStyle(this.getStyle()));
                 }
@@ -520,7 +520,9 @@ public class Component implements Cloneable, Serializable {
                             }
                         }
                     }
-                    components.get(i).append(formattedArg.toTextComponent());
+                    if (components.size() > i) {
+                        components.get(i).append(formattedArg.toTextComponent());
+                    }
                     i++;
                 }
             } else {
@@ -528,6 +530,9 @@ public class Component implements Cloneable, Serializable {
             }
         }
         components.addAll(this.children.stream().map(component -> (IFormattableTextComponent) component.toTextComponent(languageCode)).collect(Collectors.toList()));
+        if (components.isEmpty()) {
+            components.add(new StringTextComponent(""));
+        }
         IFormattableTextComponent result = components.get(0);
         for (int j = 1; j < components.size(); j++) {
             result.append(components.get(j));
