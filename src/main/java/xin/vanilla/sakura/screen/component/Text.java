37ee06d88effe5405c91b2dd6905b4b3c492ddf6
Tsuki <TsukiMaaii@gmail.com>
2025-02-28 21:37:01 +0800

    fix 1.鼠标悬浮提示文本错位 2.对象浅拷贝导致的一系列问题 3.文本渲染处理不当导致客户端崩溃的问题
    (cherry picked from commit b12aacf0e38e46dbf843ac19e90f8de1d30117ef)

--- src/main/java/xin/vanilla/sakura/screen/component/Text.java
+++ src/main/java/xin/vanilla/sakura/screen/component/Text.java
@@ -63,7 +63,7 @@ public class Text {
 
     public Text(Component text) {
         this.text = text;
-        this.hoverText = text;
+        this.hoverText = text.clone();
     }
 
     public static Text literal(String text) {
@@ -78,6 +78,17 @@ public class Text {
         return new Text()
                 .setText(this.text.clone())
                 .setHoverText(this.hoverText.clone())
+                .setHovered(this.hovered)
+                .setAlign(this.align)
+                .setHoverAlign(this.hoverAlign)
+                .setGraphics(this.graphics)
+                .setFont(this.font);
+    }
+
+    public Text copyWithoutChildren() {
+        return new Text()
+                .setText(this.text.clone().clearChildren().clearArgs())
+                .setHoverText(this.hoverText.clone().clearChildren().clearArgs())
                 .setHovered(this.hovered)
                 .setAlign(this.align)
                 .setHoverAlign(this.hoverAlign)
@@ -158,7 +169,7 @@ public class Text {
 
     public Text setText(Component text) {
         this.text = text;
-        this.hoverText = text;
+        this.hoverText = text.clone();
         return this;
     }
 
