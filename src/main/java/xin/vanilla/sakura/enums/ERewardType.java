63f865096c60ce49f9912eb4df1b3dad618f59e1
辉小月 <TsukiMaaii@gmail.com>
2026-08-01 23:48:35 +0800

    refactor(reward): register built-in grant strategies

--- src/main/java/xin/vanilla/sakura/enums/ERewardType.java
+++ src/main/java/xin/vanilla/sakura/enums/ERewardType.java
@@ -5,6 +5,8 @@ import xin.vanilla.banira.common.data.Component;
 import xin.vanilla.banira.common.enums.IEnumDescribable;
 import xin.vanilla.banira.common.util.EnumDescriptionHelper;
 import xin.vanilla.sakura.SakuraComponent;
+import xin.vanilla.sakura.api.reward.RewardTypeId;
+import xin.vanilla.sakura.api.reward.SakuraRewardTypes;
 
 import java.io.Serializable;
 
@@ -40,5 +42,28 @@ public enum ERewardType implements Serializable, IEnumDescribable {
             }
         }
         throw new IllegalArgumentException("Invalid code: " + code);
+    }
+
+    public RewardTypeId rewardTypeId() {
+        switch (this) {
+            case ITEM:
+                return SakuraRewardTypes.ITEM;
+            case EFFECT:
+                return SakuraRewardTypes.EFFECT;
+            case EXP_POINT:
+                return SakuraRewardTypes.EXPERIENCE_POINT;
+            case EXP_LEVEL:
+                return SakuraRewardTypes.EXPERIENCE_LEVEL;
+            case SIGN_IN_CARD:
+                return SakuraRewardTypes.SIGN_IN_CARD;
+            case ADVANCEMENT:
+                return SakuraRewardTypes.ADVANCEMENT;
+            case MESSAGE:
+                return SakuraRewardTypes.MESSAGE;
+            case COMMAND:
+                return SakuraRewardTypes.COMMAND;
+            default:
+                throw new IllegalStateException("Unsupported reward type: " + this);
+        }
     }
 }
