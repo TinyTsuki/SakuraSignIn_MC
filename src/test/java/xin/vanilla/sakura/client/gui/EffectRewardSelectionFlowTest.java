bf01779123a10f191e3956310d66f05715afa42c
辉小月 <TsukiMaaii@gmail.com>
2026-08-23 00:14:09 +0800

    test: 移除脆弱的结构断言

--- src/test/java/xin/vanilla/sakura/client/gui/EffectRewardSelectionFlowTest.java
+++ src/test/java/xin/vanilla/sakura/client/gui/EffectRewardSelectionFlowTest.java
@@ -47,8 +47,4 @@ public class EffectRewardSelectionFlowTest {
         assertEquals(0, reward.getAmplifier());
     }
 
-    @Test(expected = ClassNotFoundException.class)
-    public void legacySakuraEffectSelectorIsRemoved() throws Exception {
-        Class.forName("xin.vanilla.sakura.screen.EffecrSelectScreen");
-    }
 }
